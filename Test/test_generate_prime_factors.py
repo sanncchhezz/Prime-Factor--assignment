8c1dd214374e0dfeca4af5308a316637ca2ba971
Alexis Sanchez <36343677+sanncchhezz@users.noreply.github.com>
2026-01-27 09:57:08 -0500

    Step 1:
    Test was written and passed that asserts that when generate_prime_factors is called with a data type that is not an integer (e.g. a string or float), a ValueError is raised.

--- Test/test_generate_prime_factors.py
+++ Test/test_generate_prime_factors.py
@@ -1,1 +1,8 @@
+import prime
+import pytest
+
+def test_prime_number_ValueError():
+    """ tests for ValueError when input is not an integer """
+    with pytest.raises(ValueError):
+        assert prime.generate_prime_factors("hello")
 
--- prime.py
+++ prime.py
@@ -1,3 +1,8 @@
 """
 prime.py -- Write the application code here
 """
+
+def generate_prime_factors(num):
+    if type(num) is not int:
+        raise ValueError("Input must be an integer")
+    return []
